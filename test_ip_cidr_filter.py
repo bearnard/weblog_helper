eec93a75f747645f0fce6f5b359e282b86ba9ccb
Bearnard Hibbins <bearnard@praekelt.org>
2019-04-12 13:35:46 +0200

    Add tests for custom filter functions.

---
+++ test_ip_cidr_filter.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+from weblog_helper2 import ip_cidr_filter
+
+
+@pytest.fixture
+def log():
+    return (
+        '69.30.245.58', '02/Jun/2015:19:05:40 -0700',
+        'GET /wp-login.php?action=register HTTP/1.1', '404',
+        '88', 'http://redlug.com/',
+        'Opera/9.80 (Windows NT 6.2; Win64; x64)' +
+        'Presto/2.12.388 Version/12.17',
+        'redlug.com')
+
+
+def test_ip_cidr_filter_true_for_cidr(log):
+    assert ip_cidr_filter(log, '69.30.245.0/24') is True
+
+
+def test_ip_cidr_filter_true_for_ip(log):
+    assert ip_cidr_filter(log, '69.30.245.58') is True
+
+
+def test_ip_cidr_filter_false_for_wrong_ip(log):
+    assert ip_cidr_filter(log, '69.30.245.51') is False
---
+++ test_ip_filter.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+from weblog_helper import ip_filter
+
+
+@pytest.fixture
+def log():
+    return (
+        '69.30.245.58', '02/Jun/2015:19:05:40 -0700',
+        'GET /wp-login.php?action=register HTTP/1.1', '404',
+        '88', 'http://redlug.com/',
+        'Opera/9.80 (Windows NT 6.2; Win64; x64)' +
+        'Presto/2.12.388 Version/12.17',
+        'redlug.com')
+
+
+def test_ip_filter_true_for_exact_match(log):
+    assert ip_filter(log, '69.30.245.58') is True
+
+
+def test_ip_cidr_filter_false_for_wrong_ip(log):
+    assert ip_filter(log, '69.30.245.51') is False
